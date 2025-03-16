baf426b5c7b06acc162c964a53ce419052bfb538
웃기진않아 <dugudada01@naver.com>
2025-03-16 17:26:13 +0900

    FEAT: Table to Excel 배치

---
+++ src/main/java/org/example/springbatchtest/batch/FifthBatch.java
@@ -0,0 +1,80 @@
+package org.example.springbatchtest.batch;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springbatchtest.entity.BeforeEntity;
+import org.example.springbatchtest.repository.BeforeRepository;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemStreamWriter;
+import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.batch.core.Job;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class FifthBatch {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager platformTransactionManager;
+    private final BeforeRepository beforeRepository;
+
+
+    @Bean
+    public Job fifthJob() {
+
+        System.out.println("fifth job");
+
+        return new JobBuilder("fifthJob", jobRepository)
+                .start(fifthStep())
+                .build();
+    }
+    @Bean
+    public Step fifthStep() {
+
+        System.out.println("fifth step");
+
+        return new StepBuilder("fifthStep", jobRepository)
+                .<BeforeEntity, BeforeEntity> chunk(10, platformTransactionManager)
+                .reader(fifthBeforeReader())
+                .processor(fifthProcessor())
+                .writer(excelWriter())
+                .build();
+    }
+    @Bean
+    public RepositoryItemReader<BeforeEntity> fifthBeforeReader() {
+
+        return new RepositoryItemReaderBuilder<BeforeEntity>()
+                .name("beforeReader")
+                .pageSize(10)
+                .methodName("findAll")
+                .repository(beforeRepository)
+                .sorts(Map.of("id", Sort.Direction.ASC))
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<BeforeEntity, BeforeEntity> fifthProcessor() {
+
+        return item -> item;
+    }
+    @Bean
+    public ItemStreamWriter<BeforeEntity> excelWriter() {
+
+        try {
+            return new ExcelRowWriter("C:\\db study\\result.xlsx");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
--- src/main/java/org/example/springbatchtest/controller/MainController.java
+++ src/main/java/org/example/springbatchtest/controller/MainController.java
@@ -49,4 +49,13 @@ public class MainController
         jobLauncher.run(jobRegistry.getJob("fourthJob"),jobParameters);
         return "ok";
     }
+    @GetMapping("/fifth")
+    public String fifthApi(@RequestParam("value")String value) throws Exception
+    {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("date",value)
+                .toJobParameters();
+        jobLauncher.run(jobRegistry.getJob("fifthJob"),jobParameters);
+        return "ok";
+    }
 }
